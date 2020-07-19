6c8d50a2f4f34e011d5514ee07c329a6133fdc4c
davxe <princekaushal14918177h@gmail.com>
2020-07-19 23:26:42 +0530

    add exam

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,50 +1,56 @@
-const User = require('../models/user')
+const User=require('../models/user')
+const bcryptjs=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+const usersController={}
 
-module.exports.register = (req,res) => {
-    const body = req.body
-    const user = new User(body)
-    user.save()
-        .then(user=>{
-            const {_id,username,email} = user
-           res.json({_id,username,email})
-        })
-        .catch(err=>{
-            res.json(err)
+usersController.register = (req,res)=>{
+    const body=req.body
+    const user=new User(body)
+    bcryptjs.genSalt()
+        .then((salt)=>{
+            bcryptjs.hash(user.password,salt)
+                .then((encrypted)=>{
+                    user.password=encrypted
+                    user.save()
+                        .then((user)=>{
+                            res.json(user)
+                        })
+                        .catch((err)=>{
+                            res.json(err)
+                        })    
+                })
         })
 }
-
-module.exports.login = (req,res) => {
-    const body = req.body
-    let user
-    User.findByCredentials(body.email,body.password)
-        .then(userFound => {
-            user = userFound
-            return user.generateToken()
-        })
-        .then(token=>{
-            user = {_id:user._id,username:user.username,email:user.email}
-            res.json({
-                token,
-                user
-            })
-        })
-        .catch(err=>{
-            res.json(err)
-        })
-}
-
-module.exports.account = (req,res) => {
-    const {_id,username,email} = req.user
-    res.json({_id,username,email})
-}
-
-module.exports.logout = (req,res) => {
-    const {user,token} = req
-    User.findByIdAndUpdate(user._id,{$pull:{tokens:{token:token}}})
-        .then(()=>{
-            res.json({notice: 'successfully logged out'})
-        })
-        .catch(err=>{
-            res.json(err)
+usersController.login=(req,res)=>{
+    const body=req.body
+    User.findOne({email:body.email})
+        .then((user)=>{
+            if(!user)
+            {
+                res.json({
+                    error:"invalid email id or password"
+                })
+            }
+            bcryptjs.compare(body.password,user.password)
+                .then((match)=>{
+                    if(match)
+                    {
+                        const tokenData={
+                            id:user._id,
+                            email:user.email,
+                            username:user.username
+                        }
+                        const token=jwt.sign(tokenData,'Prince@123',{expiresIn:'2d'})
+                        res.json({"token":token})
+                    }
+                    else
+                    {
+                        res.json({error:"invalid email or password"})
+                    }
+                })
         })
 }
+usersController.account=(req,res)=>{
+    res.json(req.user)
+}
+module.exports=usersController
